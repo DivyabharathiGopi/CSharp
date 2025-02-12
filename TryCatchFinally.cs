3399dce3f66919d4a7b09e17a2b3ac72be8ea9b9
Divyabharathi Gopi <104125342+DivyabharathiGopi@users.noreply.github.com>
2025-02-12 11:01:34 +0530

    Update and rename TryCatch.cs to TryCatchFinally.cs

--- TryCatchFinally.cs
+++ TryCatchFinally.cs
@@ -1,20 +1,25 @@
 using System;
 
-public class TryCatch
+public class TryCatchFinally
 {
     public static void Main(string[] args)
     {
         int result =0;
         try
         {
-            Console.WriteLine("Enter the number");
+            Console.WriteLine("Enter the first number");
             int num1=int.Parse(Console.ReadLine());
-            int num2=0;
+            Console.WriteLine("Enter the second number");
+            int num2=int.Parse(Console.ReadLine());
             result = num1/num2;
         }
         catch(Exception e)
         {
              Console.WriteLine ("Error: "+e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("This finally keyword will always excutes!");
         }
         
         Console.WriteLine("Result:" +result);
